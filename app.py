659d99ee0d731b58851c15b3741ddc2761b05093
Checker700 <gps93mlk@gmail.com>
2026-01-22 21:15:05 +0000

    fix for db creation on deploy

--- app.py
+++ app.py
@@ -7,13 +7,15 @@ app = Flask(__name__)
 DB_Path = 'db/database.db'
 
 def init_db():
-    os.makedirs('db', exist_ok = True)
+    #os.makedirs('db', exist_ok = True)
     conn = sqlite3.connect(DB_Path)
     conn.execute("""
         CREATE TABLE IF NOT EXISTS games (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT NOT NULL,
-            steam_url TEXT NOT NULL
+            title TEXT,
+            steam_url TEXT,
+            description TEXT,
+            image_url TEXT
         )
     """)   
     conn.commit()
@@ -32,10 +34,11 @@ def get_games():
 @app.route('/')
 def index():
     games = get_games()
-    return render_template('index.html', games = games)
+    return render_template('index.html', games=games)
+
+init_db()
 
 if __name__ == "__main__":
-    init_db()
     port = port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
