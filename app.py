ec0f9152cb50f269ac4b653b44ce0158679c5402
Checker700 <gps93mlk@gmail.com>
2026-01-22 15:32:32 +0000

    data base created + updated code

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
 import os
+import sqlite3
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+DB_Path = 'db/database.db'
+
+def init_db():
+    os.makedirs('db', exist_ok = True)
+    conn = sqlite3.connect(DB_Path)
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS games (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            steam_url TEXT NOT NULL
+        )
+    """)   
+    conn.commit()
+    conn.close()
+
+def get_games():
+    conn = sqlite3.connect(DB_Path)
+    conn.row_factory = sqlite3.Row
+    games = conn.execute(
+        "SELECT title, steam_url FROM games"
+    ).fetchall()
+    conn.close()
+    return games
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    games = get_games()
+    return render_template(index.html, games = games)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    init_db()
+    port = port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
